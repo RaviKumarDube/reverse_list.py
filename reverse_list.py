7517447175ab59317bdb3cce450769f5f7928a56
RaviKumarDube <ramanndube@gmail.com>
2025-06-13 18:33:43 -0700

    Create reverse_list.py

---
+++ reverse_list.py
@@ -0,0 +1,7 @@
+# This program takes a list of numbers from the user and stores them in a list.
+user_wants=int(input("How many number do you want to enter:"))
+list1=[]#empty list to store numbers
+for i in range(user_wants):
+    number= int(input("Enter number ro entire in the list:"))
+    list1.append(number)
+print("Your list is:", list1)    
